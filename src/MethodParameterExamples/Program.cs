05fc3813078b8b055d082e289d52a3ca1a2f15d4
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-06-08 11:01:55 -0500

    Initial commit

---
+++ src/MethodParameterExamples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodParameterExamples {
+
+    class Program {
+
+        static void Main(string[] args) {
+            TestClass objA = new TestClass() { Id = 'A', IntegerProperty = 1 };
+            TestClass objB = new TestClass() { Id = 'B', IntegerProperty = 1 };
+            TestClass objC = new TestClass() { Id = 'C', IntegerProperty = 1 };
+
+            Update(objA);
+            Update(ref objB);
+            UpdateEx(objC);
+
+            IList<TestClass> testObjects = new List<TestClass>() { objA, objB, objC };
+
+            foreach (TestClass item in testObjects) {
+                Console.WriteLine($"ID: {item.Id}\tValue: {item.IntegerProperty}");
+            }
+        }
+
+        static void Update(TestClass obj) {
+            obj.IntegerProperty = 2;
+        }
+
+        static void UpdateEx(TestClass obj) {
+            obj = new TestClass();
+            obj.IntegerProperty = 2;
+        }
+
+        static void Update(ref TestClass obj) {
+            obj = new TestClass();
+            obj.IntegerProperty = 2;
+        }
+
+    }
+
+}
---
+++ src/MethodParameterExamples/TestClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MethodParameterExamples {
+
+	public class TestClass {
+
+		public char Id { get; set; }
+		public int IntegerProperty { get; set; }
+
+	}
+
+}
